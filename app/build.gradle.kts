97da15bdce30c1004056233301d530d2b66bf1db
PROMCRdog <64868952+PROMCRdog@users.noreply.github.com>
2026-08-02 11:59:05 +0800

    build: declare startup manifest dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.startup.runtime)
     implementation(libs.kotlinx.coroutines.android)
 
     implementation(platform(libs.compose.bom))
